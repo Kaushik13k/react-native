3445efd2084af9a7fc09cca5aeae81ef11f9ac50
Kaushik K <kaushikk@Kaushiks-MacBook-Air.local>
2024-01-25 20:44:52 +0530

    first commit

---
+++ starter101/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello world!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+export default App;
